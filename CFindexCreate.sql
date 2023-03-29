8022934770377d7ec24fa6d6cdf0e07ce6f739b3
Daniel Kruze <120228159+dkruze@users.noreply.github.com>
2023-03-29 18:46:56 -0400

    Update CFindexCreate.sql

--- CFindexCreate.sql
+++ CFindexCreate.sql
@@ -1,5 +1,4 @@
---Requirement 18 is contained in the following CREATE INDEX statements
---These indices only refer to the data in each corresponding table that is most likeley to be searched
+---These indices only refer to the data in each corresponding table that is most likeley to be searched
 --Note that while this makes some queries faster, it does make others longer depending on whether the data in the index has been updated or not
 DROP INDEX shopper_info;
 DROP INDEX product_info;
